b9c7d079a7cd11434d9bb847ba19b1ad3cafa414
Tanner Wolfe Jacobs <tanner.burghout@hotmail.com>
2017-01-19 07:42:21 -0500

    hello world

--- src/w17jan18/W17Jan18.java
+++ src/w17jan18/W17Jan18.java
@@ -15,7 +15,7 @@ public class W17Jan18 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello GitHub World!");
+      System.out.println("Hello World, My Github  account name is TannerJacobs1998");
     }
     
 }
